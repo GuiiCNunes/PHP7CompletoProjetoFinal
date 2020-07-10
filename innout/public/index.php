1616b53dd4cc2fba23a6402070d25a0c7ac81735
Guilherme Nunes <guiicnunes@gmail.com>
2020-07-10 17:46:34 -0300

    246 a 252

--- innout/public/index.php
+++ innout/public/index.php
@@ -1,13 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-
-$user = new User(['name'  => 'Lucas', 'email' => 'lucas@gg.com']);
-print_r($user);
-echo '<br>';
-$user->email = 'lucaca@mail.com';
-echo '<br>';
-print_r($user->email);
+require_once(CONTROLLER_PATH . '/login.php');
